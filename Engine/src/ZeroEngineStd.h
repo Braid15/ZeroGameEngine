7d5e8b43ac82e4a4cccdb20545f78e4fd8a44c61
Roy Atkins <royanthony0522@gmail.com>
2017-12-16 14:47:31 -0500

    Added LOG_DEBUG macro

--- Engine/src/ZeroEngineStd.h
+++ Engine/src/ZeroEngineStd.h
@@ -21,9 +21,15 @@
     #include <stdlib.h>
     #include <crtdbg.h>
     #define zero_new new(_NORMAL_BLOCK, __FILE__, __LINE__)
+    #define zero_malloc(x) _malloc_dbg((x), _NORMAL_BLOCK, __FILE__, __LINE__);
+    #define zero_free(x) _free_dbg((x), _NORMAL_BLOCK);
+    #define zero_strdup(x) strdup((x))
 #else
     #include <stdlib.h>
     #define zero_new new
+    #define zero_malloc(x) malloc((x))
+    #define zero_free(x) free((x))
+    #define zero_strdup(x) strdup((x))
 #endif
 
 #define zero_delete(x) if(x) delete x; x = nullptr;
@@ -46,9 +52,16 @@
 #include <math.h>
 #include <list>
 #include <filesystem>
+#include <typeinfo>
 #include <type_traits>
 #include <iomanip>
 #include <cassert>
 #include <strstream>
 
 #include "IZeroObject.h"
+
+
+// @TEMP - Thrown together quickly.
+// Make msg std::string so that const char* or std::string can be passed
+#define LOG_DEBUG(agent, msg) \
+    printf("[%s::%s] %s\n", agent, __func__, std::string(msg).c_str()); 
