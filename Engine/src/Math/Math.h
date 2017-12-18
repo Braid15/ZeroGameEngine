bcd2dc50e27f53d9039d452d5160bd972d805d6b
Roy Atkins <royanthony0522@gmail.com>
2017-12-17 21:49:29 -0500

    Made interface for Math but have not implemented it

---
+++ Engine/src/Math/Math.h
@@ -0,0 +1,39 @@
+#pragma once
+
+namespace ZeroEngine {
+    
+    // ---------------------------------------------
+    // Provides static interface for math functions
+    // ---------------------------------------------
+
+
+    class Math {
+    public:
+        template <typename T>
+        static T clamp(const T orig_val, const T min, const T max);
+
+        template <typename T>
+        static T power_of(const T orig_val, const int32_t power_of);
+
+        template <typename T>
+        static T square(const T orig_val);
+
+        template <typename T>
+        static T cube(const T orig_val);
+
+        template <typename T>
+        static T root_of(const T orig_val, const int32_t root_of);
+
+        template <typename T>
+        static T square_root(const T orig_val);
+
+        template <typename T>
+        static T cubed_root(const T orig_val);
+
+
+
+   private:
+        inline Math() {}
+        inline Math(const Math&) {}
+    };
+}
