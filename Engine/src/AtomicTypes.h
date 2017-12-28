667906d6bd1ee81f7e0a440798a7e86390c3db86
Roy Atkins <royanthony0522@gmail.com>
2017-12-28 14:44:01 -0500

    AtomicTypes defined

---
+++ Engine/src/AtomicTypes.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <cstdint>
+
+namespace ZeroEngine {
+
+    typedef uint8_t        u8;
+    typedef uint16_t       u16;
+    typedef uint32_t       u32;
+    typedef uint64_t       u64;
+
+    typedef int8_t         i8;
+    typedef int16_t        i16;
+    typedef int32_t        i32;
+    typedef int64_t        i64;
+
+    typedef unsigned char  byte;
+
+    typedef unsigned char  uchar;
+    typedef char16_t       char16;
+    typedef char32_t       char32;
+
+    typedef float          f32;
+    typedef double         f64;
+}
